437184bcc6b59b5c86b72c3d64cd3c26f4a4cfb5
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 22:07:39 -0700

    Update task_assignee.py

--- models/task_assignee.py
+++ models/task_assignee.py
@@ -1,15 +1,18 @@
 # models/task_assignee.py
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional
-from models.task import Task
-from models.user import User
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.task import Task
+    from models.user import User
 
 class TaskAssignee(SQLModel, table=True):
-    __tablename__ = "users"
+    __tablename__ = "task_assignees"
     __table_args__ = {"extend_existing": True}
-    id: Optional[int] = Field(default=None, primary_key=True)
-    task_id: int = Field(foreign_key="task.id")
-    user_id: int = Field(foreign_key="user.id")
 
-    task: Task = Relationship(back_populates="assignees")
-    user: User = Relationship(back_populates="task_assignments")
+    id: Optional[int] = Field(default=None, primary_key=True)
+    task_id: int = Field(foreign_key="tasks.id")
+    user_id: int = Field(foreign_key="users.id")
+
+    task: "Task" = Relationship(back_populates="assignees")
+    user: "User" = Relationship(back_populates="task_assignments")
