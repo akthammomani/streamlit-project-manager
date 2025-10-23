57e1247918d1fdce7c2a6bd99aabfb1920db4c0d
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 22:02:07 -0700

    Update project.py

--- models/project.py
+++ models/project.py
@@ -9,6 +9,8 @@ if TYPE_CHECKING:
     from models.project_member import ProjectMember
 
 class Project(SQLModel, table=True):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     description: Optional[str] = None
