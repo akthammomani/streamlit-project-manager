e6bf6ed297fcd305fe276d214d446ef7bd73ff49
Aktham Momani <aktham.momani81@gmail.com>
2025-10-23 02:07:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ st.set_page_config(
 
 
 # ---------- helpers ----------
-STATUS_OPTIONS = ["To-Do", "In-Progress", "Completed"]
+STATUS_OPTIONS = ["To-Do", "In Progress", "Done"]
 
 def parse_date(x):
     if not x:
