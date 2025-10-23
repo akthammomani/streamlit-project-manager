90c721c94b6dd7296fdaabf151478ac5aca5fe53
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 22:51:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # main.py
+from sqlmodel import SQLModel
+SQLModel.metadata.clear()
 import streamlit as st
 from db import init_db
 from ui.projects_panel import render_projects, render_new_project
