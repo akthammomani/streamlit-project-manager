1cdf6e0ef2fd76dbb49a7e42ed9d54a4b3a0c62d
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 21:39:15 -0700

    Create project.py

---
+++ models/project.py
@@ -0,0 +1,19 @@
+# models/project.py
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+import os
+from datetime import date
+from models.user import User
+from models.task import Task
+from models.project_member import ProjectMember
+
+class Project(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    join_code: str = Field(default_factory=lambda: os.urandom(3).hex(), index=True)
+
+    members: List["ProjectMember"] = Relationship(back_populates="project")
+    tasks: List["Task"] = Relationship(back_populates="project")
