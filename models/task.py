c9787cf74e223de65ceab07f9a0b36c0d27bfb63
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 21:40:04 -0700

    Create task.py

---
+++ models/task.py
@@ -0,0 +1,20 @@
+# models/task.py
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+from datetime import date
+from models.project import Project
+from models.subtask import Subtask
+from models.task_assignee import TaskAssignee
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    project_id: int = Field(foreign_key="project.id")
+    title: str
+    status: str = Field(default="Backlog")
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    percent_complete: float = Field(default=0.0)
+
+    project: Project = Relationship(back_populates="tasks")
+    subtasks: List["Subtask"] = Relationship(back_populates="task")
+    assignees: List["TaskAssignee"] = Relationship(back_populates="task")
