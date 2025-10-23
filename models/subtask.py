6a7a9812dfbb4027ac050eeef3e779c3e3dd8ef8
Aktham Momani <aktham.momani81@gmail.com>
2025-10-22 22:07:12 -0700

    Update subtask.py

--- models/subtask.py
+++ models/subtask.py
@@ -1,14 +1,17 @@
 # models/subtask.py
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 from datetime import date
-from models.task import Task
+
+if TYPE_CHECKING:
+    from models.task import Task
 
 class Subtask(SQLModel, table=True):
-    __tablename__ = "users"
+    __tablename__ = "subtasks"
     __table_args__ = {"extend_existing": True}
+
     id: Optional[int] = Field(default=None, primary_key=True)
-    task_id: int = Field(foreign_key="task.id")
+    task_id: int = Field(foreign_key="tasks.id")
     title: str
     start_date: Optional[date] = None
     end_date: Optional[date] = None
@@ -16,4 +19,4 @@ class Subtask(SQLModel, table=True):
     percent_complete: float = Field(default=0.0)
     assignee_email: Optional[str] = None
 
-    task: Task = Relationship(back_populates="subtasks")
+    task: "Task" = Relationship(back_populates="subtasks")
